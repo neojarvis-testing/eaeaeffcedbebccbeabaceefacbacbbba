cb1e165653474067ed5cc5b5fd971edf86d240e2
neojarvis <techadmin@iamneo.ai>
2025-05-25 16:02:54 +0000

    initialCommit-Boilerplate

---
+++ springappbuild/src/main/java/com/examly/springappbuild/SpringappbuildApplication.java
@@ -0,0 +1,13 @@
+package com.examly.springappbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappbuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappbuildApplication.class, args);
+	}
+
+}
---
+++ springappbuild/src/test/java/com/examly/springappbuild/SpringappbuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examly.springappbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappbuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
